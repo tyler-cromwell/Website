8498433335e8c4a8279e104f53566f5e6d938937
Tyler Cromwell <tjc6185@gmail.com>
2019-03-07 14:24:02 -0500

    Removed unnecessary directory layer

--- core/views.py
+++ core/views.py
@@ -6,20 +6,20 @@ from .models import Post
 
 
 def index(request):
-    template = loader.get_template('core/index.html')
+    template = loader.get_template('index.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
 
 def about(request):
-    template = loader.get_template('core/about.html')
+    template = loader.get_template('about.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
 
 def posts(request):
     posts = Post.objects.order_by('-published_date')
-    template = loader.get_template('core/posts.html')
+    template = loader.get_template('posts.html')
     context = {
         'posts': posts
     }
