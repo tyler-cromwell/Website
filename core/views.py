40dcf7f2e313429e273e7361a7f7055bc78289dd
Tyler Cromwell <tjc6185@gmail.com>
2019-03-09 13:07:11 -0500

    Display post author's full name

--- core/views.py
+++ core/views.py
@@ -8,14 +8,12 @@ from . import models
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render({}, request))
 
 
 def about(request):
     template = loader.get_template('about.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render({}, request))
 
 
 def posts(request):
@@ -31,6 +29,7 @@ def details(request, post_id):
     post = get_object_or_404(models.Post, pk=post_id)
     template = loader.get_template('details.html')
     context = {
-        'post': post
+        'post': post,
+        'author': post.author
     }
     return HttpResponse(template.render(context, request))
