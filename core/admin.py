b99a0c8bb9c8e052935e9cba0f1b6ade085efaf0
Tyler Cromwell <tjc6185@gmail.com>
2019-04-22 14:34:41 -0400

    Added new models for associating images and source code with posts

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea
 
 from .models import Post
+from .models import Image
+from .models import Code
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows':10, 'cols':80})},
+    }
+
+    # Home > Core > Posts page
+    list_display = ('id', 'category', 'title', 'created_date', 'published_date', 'featured', 'enabled')
+    list_filter = ('category', 'featured', 'enabled')
+
+
+class ImageAdmin(admin.ModelAdmin):
+    # Home > Core > Images page
+    list_display = ('id', 'image', 'post', 'order')
+    list_filter = ('post',)
+
+
+class CodeAdmin(admin.ModelAdmin):
+    # Home > Core > Code page
+    list_display = ('id', 'post', 'language', 'order')
+    list_filter = ('post', 'language')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Code, CodeAdmin)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 import enum
 
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
 
@@ -17,12 +18,25 @@ CHOICES = (
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    category = models.SmallIntegerField(choices=CHOICES, default=Category.PROGRAMMING.value)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    category = models.SmallIntegerField(
+        choices=CHOICES,
+        default=Category.PROGRAMMING.value
+    )
+
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateField(default=timezone.now)
     published_date = models.DateField(blank=True, null=True)
+
+    featured = models.BooleanField(
+        help_text='Distinguish from others',
+        default=False
+    )
     enabled = models.BooleanField(
         help_text='Show/hide post',
         default=False
@@ -30,3 +44,41 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE
+    )
+    image = models.CharField(
+        help_text='Relative path name of the file',
+        max_length=100
+    )
+    order = models.IntegerField(
+        default=0,
+        help_text='Order of appearance on page (descending)'
+    )
+
+    def __str__(self):
+        return self.image
+
+
+class Code(models.Model):
+    class Meta:
+        verbose_name = 'Source Code'
+        verbose_name_plural = 'Source Code'
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE
+    )
+    language = models.CharField(
+        blank=False,
+        max_length=40
+    )
+    order = models.IntegerField(
+        default=0,
+        help_text='Order of appearance on page (descending)'
+    )
+
+    text = models.TextField()
